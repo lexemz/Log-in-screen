30963448f2d756da5c6093adecb2c48fea657cad
Igor Buzykin <lexemz@gmail.com>
2021-08-24 16:40:35 +0300

    u UserView VC

--- Log-in screen/LogInViewController.swift
+++ Log-in screen/LogInViewController.swift
@@ -9,29 +9,33 @@ import UIKit
 
 class LogInViewController: UIViewController {
 
+    // MARK: - IB Outlets
     @IBOutlet var loginButton: UIView!
     
     @IBOutlet var usernameTextfield: UITextField!
     @IBOutlet var passwordTextfield: UITextField!
     
+    // MARK: - Public Properties
     let userName = "user"
     let userPassword = "qwe"
     
+    // MARK: - Life Cycles Methods
     override func viewDidLoad() {
         super.viewDidLoad()
         loginButton.layer.cornerRadius = 5
+        
     }
     
     override func shouldPerformSegue(withIdentifier identifier: String, sender: Any?) -> Bool {
-        guard let name = usernameTextfield.text, !name.isEmpty else {
+        guard let userName = usernameTextfield.text, !userName.isEmpty else {
             print("username shit!")
             return false
         }
-        guard let pass = passwordTextfield.text, !pass.isEmpty else {
+        guard let userPass = passwordTextfield.text, !userPass.isEmpty else {
             print("pass shit!")
             return false
         }
-        if pass != userPassword || name != userName {
+        if userPass != userPassword || userName != userName {
             print("wrong password or username")
             return false
         }
@@ -39,9 +43,11 @@ class LogInViewController: UIViewController {
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        
+        guard let welcomeVC = segue.destination as? WelcomeViewController else { return }
+        welcomeVC.userName = userName
     }
-    
+
+    // MARK: - IB Actions
     @IBAction func forgotUserNameButtonPressed() {
     }
     
