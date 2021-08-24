52dba196889ee145d9324118c718bc73efc71e50
Igor Buzykin <lexemz@gmail.com>
2021-08-24 23:49:22 +0300

    update WelcomeViewController.swift

--- Log-in screen/WelcomeViewController.swift
+++ Log-in screen/WelcomeViewController.swift
@@ -9,12 +9,27 @@ import UIKit
 
 class WelcomeViewController: UIViewController {
     @IBOutlet var userWelcomeLabel: UILabel!
-
+    
+    @IBOutlet var logOutButton: UIButton!
+    
     var userName: String!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         userWelcomeLabel.text = "Welcome, " + userName.firstUppercased
+        logOutButton.layer.cornerRadius = 10
+        colorizeBackground()
+    }
+    
+    private func colorizeBackground() {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.frame = self.view.bounds
+        
+        gradientLayer.colors = [
+            UIColor(red:138/255.0, green:48/255.0, blue:127/255.0, alpha:1.0).cgColor,
+            UIColor(red:77/255.0, green:81/255.0, blue:142/255.0, alpha:1.0).cgColor
+        ]
+        self.view.layer.insertSublayer(gradientLayer, at: 0)
     }
 }
 
